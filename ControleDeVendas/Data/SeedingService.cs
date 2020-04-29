4b4d46b1017a22a5927a6432a92385d3ca1208f5
bruno <brunodoliveirapereira@gmail.com>
2020-04-29 12:00:05 -0300

    Lancando Dados nas Tabelas

---
+++ ControleDeVendas/Data/SeedingService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ControleDeVendas.Models;
+using ControleDeVendas.Models.Enums;
+
+namespace ControleDeVendas.Data
+{
+    public class SeedingService
+    {
+        private ControleDeVendasContext _context;
+
+        public SeedingService(ControleDeVendasContext context)
+        {
+            _context = context;
+        }
+        public void Seed()
+        {
+            if(_context.Department.Any() || _context.Seller.Any() || _context.SalesRecord.Any())
+            {
+                return; // banco ja foi populado
+            }
+            Department d1 = new Department(1, "HERTZ");
+            Department d2 = new Department(2, "FRANCHISING");
+            Department d3 = new Department(3, "GESTÃO DE FROTAS");
+            Department d4 = new Department(4, "SEMINOVOS");
+
+            Seller s1 = new Seller(1, "Jose Luiz", "Jose@gmail.com", new DateTime(1998, 4, 21), 1000.0, d1);
+            Seller s2 = new Seller(2, "Jonh House", "Jonh@gmail.com", new DateTime(1979, 12, 31), 3500.0, d2);
+            Seller s3 = new Seller(3, "Julia Silva", "Julia@gmail.com", new DateTime(1988, 1, 15), 2200.0, d1);
+            Seller s4 = new Seller(4, "Ana Gomes", "Ana@gmail.com", new DateTime(1993, 11, 30), 3000.0, d4);
+            Seller s5 = new Seller(5, "Maria Luiza", "Maria@gmail.com", new DateTime(2000, 1, 9), 4000.0, d3);
+            Seller s6 = new Seller(6, "Alex OLiveira", "Alex@gmail.com", new DateTime(1997, 3, 4), 3000.0, d2);
+
+            SalesRecord r1 = new SalesRecord(1, new DateTime(2020, 01, 25), 6000.0, SaleStatus.Billed, s1);
+            SalesRecord r2 = new SalesRecord(2, new DateTime(2020, 01, 4), 12000.0, SaleStatus.Billed, s5);
+            SalesRecord r3 = new SalesRecord(3, new DateTime(2020, 01, 13), 3000.0, SaleStatus.Canceled, s4);
+            SalesRecord r4 = new SalesRecord(4, new DateTime(2020, 01, 1), 7000.0, SaleStatus.Billed, s1);
+            SalesRecord r5 = new SalesRecord(5, new DateTime(2020, 01, 21), 4000.0, SaleStatus.Billed, s3);
+            SalesRecord r6 = new SalesRecord(6, new DateTime(2020, 01, 15), 3000.0, SaleStatus.Billed, s1);
+            SalesRecord r7 = new SalesRecord(7, new DateTime(2020, 01, 28), 10000.0, SaleStatus.Billed, s2);
+            SalesRecord r8 = new SalesRecord(8, new DateTime(2020, 01, 11), 3000.0, SaleStatus.Billed, s4);
+            SalesRecord r9 = new SalesRecord(9, new DateTime(2020, 01, 14), 10000.0, SaleStatus.Pending, s6);
+            SalesRecord r10 = new SalesRecord(10, new DateTime(2020, 01, 7), 8000.0, SaleStatus.Billed, s6);
+            SalesRecord r11 = new SalesRecord(11, new DateTime(2020, 01, 13), 5000.0, SaleStatus.Billed, s2);
+            SalesRecord r12 = new SalesRecord(12, new DateTime(2020, 01, 25), 6000.0, SaleStatus.Pending, s3);
+            SalesRecord r13 = new SalesRecord(13, new DateTime(2020, 02, 29), 9000.0, SaleStatus.Billed, s4);
+            SalesRecord r14 = new SalesRecord(14, new DateTime(2020, 02, 4), 6000.0, SaleStatus.Billed, s5);
+            SalesRecord r15 = new SalesRecord(15, new DateTime(2020, 02, 12), 5000.0, SaleStatus.Billed, s1);
+            SalesRecord r16 = new SalesRecord(16, new DateTime(2020, 02, 5), 1000.0, SaleStatus.Billed, s4);
+            SalesRecord r17 = new SalesRecord(17, new DateTime(2020, 02, 1), 14000.0, SaleStatus.Billed, s1);
+            SalesRecord r18 = new SalesRecord(18, new DateTime(2020, 02, 24), 3000.0, SaleStatus.Billed, s3);
+            SalesRecord r19 = new SalesRecord(19, new DateTime(2020, 02, 22), 8000.0, SaleStatus.Billed, s5);
+            SalesRecord r20 = new SalesRecord(20, new DateTime(2020, 03, 15), 5000.0, SaleStatus.Billed, s6);
+            SalesRecord r21 = new SalesRecord(21, new DateTime(2020, 03, 17), 10000.0, SaleStatus.Billed, s2);
+            SalesRecord r22 = new SalesRecord(22, new DateTime(2020, 03, 24), 2000.0, SaleStatus.Billed, s4);
+            SalesRecord r23 = new SalesRecord(23, new DateTime(2020, 03, 19), 12000.0, SaleStatus.Canceled, s2);
+            SalesRecord r24 = new SalesRecord(24, new DateTime(2020, 03, 12), 7000.0, SaleStatus.Billed, s5);
+            SalesRecord r25 = new SalesRecord(25, new DateTime(2020, 03, 31), 3000.0, SaleStatus.Billed, s3);
+            SalesRecord r26 = new SalesRecord(26, new DateTime(2020, 04, 6), 5000.0, SaleStatus.Billed, s4);
+            SalesRecord r27 = new SalesRecord(27, new DateTime(2020, 04, 13), 2000.0, SaleStatus.Pending, s1);
+            SalesRecord r28 = new SalesRecord(28, new DateTime(2020, 04, 7), 7000.0, SaleStatus.Billed, s3);
+            SalesRecord r29 = new SalesRecord(29, new DateTime(2020, 04, 23), 11000.0, SaleStatus.Billed, s5);
+            SalesRecord r30 = new SalesRecord(30, new DateTime(2020, 04, 12), 2000.0, SaleStatus.Billed, s2);
+
+            _context.Department.AddRange(d1, d2, d3, d4);
+
+            _context.Seller.AddRange(s1, s2, s3, s4, s5, s6);
+
+            _context.SalesRecord.AddRange(
+                r1, r2, r3, r4, r5, r6, r7, r8, r9, r10,
+                r11, r12, r13, r14, r15, r16, r17, r18, r19, r20,
+                r21, r22, r23, r24, r25, r26, r27, r28, r29, r30
+            );
+
+            _context.SaveChanges();
+        }
+    }
+}
--- ControleDeVendas/Startup.cs
+++ ControleDeVendas/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using ControleDeVendas.Models;
+using ControleDeVendas.Data;
 
 namespace ControleDeVendas
 {
@@ -39,14 +40,17 @@ namespace ControleDeVendas
             services.AddDbContext<ControleDeVendasContext>(options =>
                       options.UseMySql(Configuration.GetConnectionString("ControleDeVendasContext"), builder =>
                       builder.MigrationsAssembly("ControleDeVendas")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
