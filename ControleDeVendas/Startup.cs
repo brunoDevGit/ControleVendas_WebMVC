b4c499b10e06c6b527082dae3c5ad7e1534ad827
bruno <brunodoliveirapereira@gmail.com>
2020-04-29 09:37:24 -0300

    Migrations Connect DB

--- ControleDeVendas/Startup.cs
+++ ControleDeVendas/Startup.cs
@@ -37,7 +37,8 @@ namespace ControleDeVendas
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<ControleDeVendasContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ControleDeVendasContext")));
+                      options.UseMySql(Configuration.GetConnectionString("ControleDeVendasContext"), builder =>
+                      builder.MigrationsAssembly("ControleDeVendas")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
